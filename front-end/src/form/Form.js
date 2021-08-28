6ce838aa8dc804e3c4e79b67912294e38d77b063
songemma <xemmasongx@gmail.com>
2021-08-28 14:41:40 -0400

    css for the form

--- front-end/src/form/Form.js
+++ front-end/src/form/Form.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 
-import './Form.css';
+// import './Form.css';
+import './style.css';
+import logo from './signup-image.jpeg'
 
 
 const Form = () => {
@@ -11,8 +13,74 @@ const Form = () => {
     const[bio, setBio] = useState('');
 
     return(
-        <div class = "box">
-        <div id="form-box">
+        <div class = "main">
+            <section class = "signup">
+                <div class = "container">
+                    <div class = "signup-content">
+                        <div class = "signup-form">
+                            <h2 class="form-title">Input your Personal Info</h2>
+                            <form method="POST" class="register-form" id="register-form">
+                                <div class="form-group">
+                                    <input type="text" name="name" id="name" placeholder="First Name" value={firstName} onChange={(event)=>setFirstName(event.target.value)}/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="name" id="name" placeholder="Last Name" value={lastName} onChange={(event)=>setLastName(event.target.value)}/>
+                                </div>
+                                <div class="form-group">
+                                    <select value={language} name = "name" id="name" onChange={(event)=>setLanguage(event.target.value)} >
+                                        <option selected value="">---Select Language ---</option>
+                                        <option selected value="Arabic">Arabic</option>
+                                        <option value="Australian English">Australian English</option>
+                                        <option value="Brazilian Portuguese">Brazilian Portuguese</option>
+                                        <option value="British English">British English</option>
+                                        <option value="Canadian French">Canadian French</option>
+                                        <option value="Castilian Spanish">Castilian Spanish</option>
+                                        <option value="Chinese Mandarin">Chinese Mandarin</option>
+                                        <option value="Danish">Danish</option>
+                                        <option value="Dutch">Dutch</option>
+                                        <option value="French">French</option>
+                                        <option value="German">German</option>
+                                        <option value="Hindi or Indian English">Hindi or Indian English</option>
+                                        <option value="Icelandic">Icelandic</option>
+                                        <option value="Italian">Italian</option>
+                                        <option value="Japanese">Japanese</option>
+                                        <option value="Korean">Korean</option>
+                                        <option value="Mexican Spanish">Mexican Spanish</option>
+                                        <option value="Norwegian">Norwegian</option>
+                                        <option value="Polish">Polish</option>
+                                        <option value="Portuguese">Portuguese</option>
+                                        <option value="Romanian">Romanian</option>
+                                        <option value="Russian">Russian</option>
+                                        <option value="Swedish">Swedish</option>
+                                        <option value="Turkish">Turkish</option>
+                                        <option value="US English">US English</option>
+                                        <option value="US Spanish">US Spanish</option>
+                                        <option value="Welsh">Welsh</option>
+                                        <option value="Welsh English">Welsh English</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="name" id="name" placeholder="Pronouns" value={pronouns} onChange={(event)=>setPronouns(event.target.value)}/>
+                                </div>
+                                <div class="form-group">
+                                    {/* <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" /> */}
+                                    <textarea name = "name" id = "name" placeholder="A little about me!" rows= "4" cols = "50" value={bio} onChange={(event)=>setBio(event.target.value)}/>
+
+                                </div>
+                                <div class="form-group form-button">
+                                    <input type="submit" name="signup" id="signup" class="form-submit" value="Submit" />
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="signup-image">
+                        <img src= {logo} alt = " meaningful"/>
+                    </div>
+                    </div>
+                </div>
+            </section>
+
+        {/* <div id="form-box">
             <h3>Input your Personal Information</h3>
             <label class="content">
                 <input placeholder="First Name" type = "text" value={firstName} onChange={(event)=>setFirstName(event.target.value)} />
@@ -68,7 +136,7 @@ const Form = () => {
             <div class = "btn">
             <button class = "content">Submit</button>
             </div>
-        </div>
+        </div> */}
         </div>
 
     )
